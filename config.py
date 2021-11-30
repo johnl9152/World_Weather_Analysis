75461f9e8dcfb20b7149bc9837a45cb554ae2f1c
John Lee <johnl9152@gmail.com>
2021-11-30 12:29:59 -0600

    Finishing Module 6

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-weather_api_key="77b5f013dde17970fb37d6415a3c845f"
+# Open Weather Platform API Key
+weather_api_key = "77b5f013dde17970fb37d6415a3c845f"
+
+# Google API Key
+g_key = "AIzaSyCc3lnfE5C52ETgYUJM9-Q7foG0r2e2g18"
